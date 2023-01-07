3ea4cf4e4467ede135109b0e06c5adc13799b358
jun094 <jun094@naver.com>
2023-01-07 16:58:21 +0900

    feat: 로그인 뼈대코드 작성

---
+++ pages/main/index.tsx
@@ -0,0 +1,13 @@
+export default function Main() {
+    const name = "blabla";
+    const id = "blabla";
+
+    return (
+        <main>
+            <h1>메인페이지입니다.</h1>
+
+            <p>사용자 이름 : {name}</p>
+            <p>사용자 아이디 : {id}</p>
+        </main>
+    );
+}
