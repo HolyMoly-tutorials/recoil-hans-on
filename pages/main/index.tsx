4034ff5e30a21204c7ab03e958b14a518734d472
jun094 <jun094@naver.com>
2023-01-07 18:23:27 +0900

    feat: useRecoilValue

--- pages/main/index.tsx
+++ pages/main/index.tsx
@@ -1,13 +1,15 @@
+import { useRecoilValue } from "recoil";
+import { IUser, user } from "constants/atom";
+
 export default function Main() {
-    const name = "blabla";
-    const id = "blabla";
+  const { id, name } = useRecoilValue<IUser>(user);
 
-    return (
-        <main>
-            <h1>메인페이지입니다.</h1>
+  return (
+    <main>
+      <h1>메인페이지입니다.</h1>
 
-            <p>사용자 이름 : {name}</p>
-            <p>사용자 아이디 : {id}</p>
-        </main>
-    );
+      <p>사용자 이름 : {name}</p>
+      <p>사용자 아이디 : {id}</p>
+    </main>
+  );
 }
